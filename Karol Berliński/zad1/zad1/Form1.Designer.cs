5c1e4095f0ec9f5864684e05080d9861c5287230
Fuze0B <115667965+Fuze0B@users.noreply.github.com>
2026-02-18 12:36:51 +0100

    Add files via upload

---
+++ Karol Berliński/zad1/zad1/Form1.Designer.cs
@@ -0,0 +1,357 @@
+﻿namespace zad1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.studentToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.daneOsoboweToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.inneInformacjeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.hasłoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.button2 = new System.Windows.Forms.Button();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.studentToolStripMenuItem,
+            this.hasłoToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // studentToolStripMenuItem
+            // 
+            this.studentToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.daneOsoboweToolStripMenuItem,
+            this.inneInformacjeToolStripMenuItem});
+            this.studentToolStripMenuItem.Name = "studentToolStripMenuItem";
+            this.studentToolStripMenuItem.Size = new System.Drawing.Size(60, 20);
+            this.studentToolStripMenuItem.Text = "Student";
+            // 
+            // daneOsoboweToolStripMenuItem
+            // 
+            this.daneOsoboweToolStripMenuItem.Name = "daneOsoboweToolStripMenuItem";
+            this.daneOsoboweToolStripMenuItem.Size = new System.Drawing.Size(157, 22);
+            this.daneOsoboweToolStripMenuItem.Text = "Dane osobowe";
+            this.daneOsoboweToolStripMenuItem.Click += new System.EventHandler(this.daneOsoboweToolStripMenuItem_Click);
+            // 
+            // inneInformacjeToolStripMenuItem
+            // 
+            this.inneInformacjeToolStripMenuItem.Name = "inneInformacjeToolStripMenuItem";
+            this.inneInformacjeToolStripMenuItem.Size = new System.Drawing.Size(157, 22);
+            this.inneInformacjeToolStripMenuItem.Text = "Inne informacje";
+            this.inneInformacjeToolStripMenuItem.Click += new System.EventHandler(this.inneInformacjeToolStripMenuItem_Click);
+            // 
+            // hasłoToolStripMenuItem
+            // 
+            this.hasłoToolStripMenuItem.Name = "hasłoToolStripMenuItem";
+            this.hasłoToolStripMenuItem.Size = new System.Drawing.Size(49, 20);
+            this.hasłoToolStripMenuItem.Text = "Hasło";
+            this.hasłoToolStripMenuItem.Click += new System.EventHandler(this.hasłoToolStripMenuItem_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(15, 115);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(59, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Nazwisko: ";
+            this.label1.Visible = false;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(13, 132);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.Visible = false;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Items.AddRange(new object[] {
+            "Uniwersytet",
+            "Politechnik",
+            "Medycyna",
+            "Ekonomia"});
+            this.listBox1.Location = new System.Drawing.Point(13, 190);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(120, 95);
+            this.listBox1.TabIndex = 3;
+            this.listBox1.Visible = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(15, 174);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(54, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Uczelnia: ";
+            this.label2.Visible = false;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(15, 303);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(93, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Tryb studiowania: ";
+            this.label3.Visible = false;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(13, 319);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(64, 17);
+            this.radioButton1.TabIndex = 6;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Dzienne";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.Visible = false;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(105, 319);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(67, 17);
+            this.radioButton2.TabIndex = 7;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Zaoczne";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.Visible = false;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(93, 342);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 9;
+            this.button2.Text = "Do pliku";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Visible = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.Items.AddRange(new object[] {
+            "Francuski",
+            "Chiński",
+            "Angielski",
+            "Hiszpański",
+            "Włoski",
+            "Niemiecki"});
+            this.listBox2.Location = new System.Drawing.Point(629, 33);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.SelectionMode = System.Windows.Forms.SelectionMode.MultiSimple;
+            this.listBox2.Size = new System.Drawing.Size(120, 95);
+            this.listBox2.TabIndex = 10;
+            this.listBox2.Visible = false;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(588, 33);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(39, 13);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "Języki:";
+            this.label4.Visible = false;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(588, 166);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(41, 13);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "Hobby:";
+            this.label5.Visible = false;
+            // 
+            // checkedListBox1
+            // 
+            this.checkedListBox1.FormattingEnabled = true;
+            this.checkedListBox1.Items.AddRange(new object[] {
+            "Sport",
+            "Muzyka",
+            "Polityka",
+            "Gotowanie"});
+            this.checkedListBox1.Location = new System.Drawing.Point(629, 166);
+            this.checkedListBox1.Name = "checkedListBox1";
+            this.checkedListBox1.Size = new System.Drawing.Size(120, 94);
+            this.checkedListBox1.TabIndex = 14;
+            this.checkedListBox1.Visible = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 342);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "Zatwierdź";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Visible = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(629, 266);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 15;
+            this.button3.Text = "Zatwierdź";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Visible = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(293, 308);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(81, 13);
+            this.label6.TabIndex = 16;
+            this.label6.Text = "Długość hasła:";
+            this.label6.Visible = false;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(380, 303);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 17;
+            this.textBox2.Visible = false;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(332, 333);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(42, 13);
+            this.label7.TabIndex = 18;
+            this.label7.Text = "Hasło: ";
+            this.label7.Visible = false;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(380, 330);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.ReadOnly = true;
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 19;
+            this.textBox3.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.checkedListBox1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem studentToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem daneOsoboweToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem inneInformacjeToolStripMenuItem;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ToolStripMenuItem hasłoToolStripMenuItem;
+        private System.Windows.Forms.CheckedListBox checkedListBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox textBox3;
+    }
+}
+
---
+++ Karol Berliński/zad1/zad1/Form1.cs
@@ -0,0 +1,179 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace zad1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void daneOsoboweToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            label1.Visible = true;
+            label2.Visible = true;
+            label3.Visible = true;
+            textBox1.Visible = true;
+            listBox1.Visible = true;
+            radioButton1.Visible = true;
+            radioButton2.Visible = true;
+            button1.Visible = true;
+            button2.Visible = true;
+
+            textBox1.Text = "";
+            listBox1.SelectedItem = null;
+            radioButton1.Checked = false;
+            radioButton2.Checked = false;
+
+            label4.Visible = false;
+            label5.Visible = false;
+            listBox2.Visible = false;
+            checkedListBox1.Visible = false;
+            button3.Visible = false;
+
+            textBox2.Visible = false;
+            textBox3.Visible = false;
+            label6.Visible = false;
+            label7.Visible = false;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string nazwisko = textBox1.Text;
+            string uczelnia = listBox1.SelectedItem.ToString();
+            string tryb = "";
+
+            if (radioButton1.Checked)
+                tryb = "Dzienne";
+            else if (radioButton2.Checked)
+                tryb = "Zaoczne";
+
+            MessageBox.Show("Nazwisko: " + nazwisko +
+                            "\nUczelnia: " + uczelnia +
+                            "\nTryb: " + tryb);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            string nazwisko = textBox1.Text;
+            string uczelnia = listBox1.SelectedItem.ToString();
+            string tryb = "";
+
+            if (radioButton1.Checked)
+                tryb = "Dzienne";
+            else if (radioButton2.Checked)
+                tryb = "Zaoczne";
+
+            string dane = "Nazwisko: " + nazwisko +
+                          "\nUczelnia: " + uczelnia +
+                          "\nTryb: " + tryb;
+
+            File.AppendAllText("zapis.txt", dane);
+            MessageBox.Show("Zapisano do pliku");
+        }
+
+        private void inneInformacjeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            label4.Visible = true;
+            label5.Visible = true;
+            listBox2.Visible = true;
+            checkedListBox1.Visible = true;
+            button3.Visible = true;
+
+            listBox2.SelectedItems.Clear();
+            checkedListBox1.CheckedItems = false; 
+
+            label1.Visible = false;
+            label2.Visible = false;
+            label3.Visible = false;
+            textBox1.Visible = false;
+            listBox1.Visible = false;
+            radioButton1.Visible = false;
+            radioButton2.Visible = false;
+            button1.Visible = false;
+            button2.Visible = false;
+
+            textBox2.Visible = false;
+            textBox3.Visible = false;
+            label6.Visible = false;
+            label7.Visible = false;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string jezyki = "Języki: ";
+            foreach (var jezyk in listBox2.SelectedItems)
+            {
+                jezyki += jezyk.ToString() + ", ";
+            }
+
+            string hobby = "Hobby: ";
+            foreach (var hobby1 in checkedListBox1.CheckedItems)
+            {
+                hobby += hobby1.ToString() + ", ";
+            }
+
+            MessageBox.Show(jezyki + "\n" + hobby);
+        }
+
+        private void hasłoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            textBox2.Visible = true;
+            textBox3.Visible = true;
+            label6.Visible = true;
+            label7.Visible = true;
+
+            label4.Visible = false;
+            label5.Visible = false;
+            listBox2.Visible = false;
+            checkedListBox1.Visible = false;
+            button3.Visible = false;
+
+            label1.Visible = false;
+            label2.Visible = false;
+            label3.Visible = false;
+            textBox1.Visible = false;
+            listBox1.Visible = false;
+            radioButton1.Visible = false;
+            radioButton2.Visible = false;
+            button1.Visible = false;
+            button2.Visible = false;
+
+            textBox2.Text = File.ReadAllText("haslodl.txt");
+            int dlugosc = int.Parse(textBox2.Text);
+
+            Random rnd = new Random();
+
+            string male = "abcdefghijklmnopqrstuvwxyz";
+            string duze = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            string cyfry = "0123456789";
+            string znaki = "!@#$%^&*";
+
+            char[] haslo = new char[dlugosc];
+
+            haslo[0] = duze[rnd.Next(duze.Length)];
+
+            haslo[1] = cyfry[rnd.Next(cyfry.Length)];
+
+            haslo[2] = znaki[rnd.Next(znaki.Length)];
+
+            for (int i = 3; i < dlugosc; i++)
+            {
+                haslo[i] = male[rnd.Next(male.Length)];
+            }
+            string hasloString = new string(haslo);
+            textBox3.Text = hasloString;
+        }
+    }
+}
